f081a2a0a1eb4d339d32025ed9f526ee8d31f2b0
luhde12 <anandaputri.038.trpl@gmail.com>
2024-07-17 18:42:26 +0800

    mengirim notifikasi sign up ke email pengguna

--- application/controllers/Cregister.php
+++ application/controllers/Cregister.php
@@ -3,24 +3,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Cregister extends CI_Controller {
 
-    public function __construct()
-    {
+    public function __construct() {
         parent::__construct();
-        $this->load->model('mregister');
+        $this->load->model('Mregister');
     }
 
-    public function formregister()
-    {
+    public function formregister() {
         $this->load->view('register');
     }
 
-    public function simpandata()
-	{
-		$this->mregister->simpandata();
-		redirect('clogin/formlogin');	
-	}
-
-   
-
+    public function simpandata() {
+        $result = $this->Mregister->simpandata();
+        if ($result) {
+            $this->session->set_flashdata('pesan', 'Registrasi berhasil, silakan cek email untuk notifikasi.');
+        } else {
+            $this->session->set_flashdata('pesan', 'Registrasi berhasil, tetapi gagal mengirim email notifikasi.');
+        }
+        redirect('cregister/formregister');
+    }
 }
 ?>
--- application/models/Mregister.php
+++ application/models/Mregister.php
@@ -1,28 +1,50 @@
 <?php
-    class Mregister extends CI_Model{
-        function simpandata()
-		{
-			$data=$_POST;
-			$Username =$data['username'];
-			
+// application/models/Mregister.php
 
-			$this->db->where('username', $Username);
-			$query = $this->db->get('tbpengguna');
+class Mregister extends CI_Model {
+    public function simpandata() {
+        $data = $_POST;
+        $Username = $data['username'];
 
-			if($query->num_rows()>0){
-				$this->session->set_flashdata('pesan','Email sudah terdaftar');
-				redirect('cregister/formregister');
-			 }
-			 else{
-				$data=$_POST;
-				$data['level']="User";
-				$this->db->insert('tbpengguna', $data);
-				redirect('clogin/formlogin');
-			 }
+        $this->db->where('username', $Username);
+        $query = $this->db->get('tbpengguna');
 
-		}
+        if ($query->num_rows() > 0) {
+            $this->session->set_flashdata('pesan', 'Username sudah terdaftar');
+            redirect('cregister/formregister');
+        } else {
+            $dataToInsert = [
+                'nama_lengkap' => $data['nama_lengkap'],
+                'username' => $data['username'],
+                'password' => $data['password'],
+                'jenis_kelamin' => $data['jenis_kelamin'],
+                'nomor_hp' => $data['nomor_hp'],
+                'level' => 'User'
+            ];
+            $this->db->insert('tbpengguna', $dataToInsert);
 
+            return $this->sendNotificationEmail($data['username'], $data['nama_lengkap']);
+        }
     }
 
+    public function sendNotificationEmail($toEmail, $toName) {
+        require_once APPPATH . 'PHPMailerConfig.php';
 
+        $mail = PHPMailerConfig::getMailer();
+        if ($mail) {
+            try {
+                $mail->addAddress($toEmail, $toName);
+                $mail->Subject = 'Notification Email NangAyan Hotel';
+                $mail->Body    = 'Registration Success. Thank you for registering with us.';
+                $mail->AltBody = 'Registration Success. Thank you for registering with us.';
+
+                $mail->send();
+                return true;
+            } catch (Exception $e) {
+                return false;
+            }
+        }
+        return false;
+    }
+}
 ?>
